64175baa3cf72a548ffbf1fadada82a446f2169a
Fauzan <fauzannurdiansyah13@gmail.com>
2023-09-27 15:30:58 +0700

    initial commit

---
+++ Jurnal01.java
@@ -0,0 +1,13 @@
+public class Jurnal01 {
+    public static void main(String[] args) {
+        String nama, kelas, nim;
+        nama = "Evansius Rafael Simbolon";
+        kelas = "D3IF-47-03";
+        nim = "607062300107";
+
+        System.out.println("Nama\t:"+nama);
+        System.out.println("Kelas\t:"+kelas);
+        System.out.println("NIM\t:"+nim);
+    }
+    
+}
